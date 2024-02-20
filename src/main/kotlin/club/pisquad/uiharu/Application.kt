664a5796f9b051bd8b11b5d8d60086ddbcb3e157
Kening Hu <kening.hu@outlook.com>
2024-02-20 21:09:18 +0800

    delete starter module

--- src/main/kotlin/club/pisquad/uiharu/Application.kt
+++ src/main/kotlin/club/pisquad/uiharu/Application.kt
@@ -1,6 +1,5 @@
 package club.pisquad.uiharu
 
-import club.pisquad.uiharu.plugins.*
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
@@ -8,5 +7,4 @@ fun main(args: Array<String>) {
 }
 
 fun Application.module() {
-    configureRouting()
 }
