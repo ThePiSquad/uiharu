140d6b24076a9fc768ff3eef7b94196d6ba9f04a
Kening Hu <kening.hu@outlook.com>
2024-02-20 21:08:12 +0800

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "club.pisquad.uiharu.uiharu"
---
+++ src/main/kotlin/club/pisquad/uiharu/Application.kt
@@ -0,0 +1,12 @@
+package club.pisquad.uiharu
+
+import club.pisquad.uiharu.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
