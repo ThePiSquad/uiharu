bb39b126827edbd83da804181ebfd3c8fdc154f8
hukening <kening.hu@outlook.com>
2024-03-03 10:15:56 +0800

    basic api call process

--- src/main/kotlin/club/pisquad/uiharu/qqbot/command/AskAICommand.kt
+++ src/main/kotlin/club/pisquad/uiharu/qqbot/command/AskAICommand.kt
@@ -1,10 +1,34 @@
 package club.pisquad.uiharu.qqbot.command
 
+import club.pisquad.uiharu.AppConfiguration
 import club.pisquad.uiharu.qqbot.api.QQBotApi
 import club.pisquad.uiharu.qqbot.api.dto.SendChannelMessageRequest
 import club.pisquad.uiharu.qqbot.websocket.dto.MessageCreateEvent
+import com.alibaba.dashscope.aigc.generation.Generation
+import com.alibaba.dashscope.aigc.generation.models.QwenParam
+import com.alibaba.dashscope.common.MessageManager
+import com.alibaba.dashscope.utils.Constants
 import io.ktor.client.statement.*
 
+internal object AskAiUtils {
+    private val gen: Generation = Generation()
+    private val messageManager: MessageManager = MessageManager()
+
+    init {
+        Constants.apiKey = AppConfiguration.getString("dashScopeApiKey")
+    }
+
+    fun getResponse(message: String): String {
+        val param = QwenParam.builder().model(Generation.Models.QWEN_PLUS).messages(messageManager.get())
+            .resultFormat(QwenParam.ResultFormat.MESSAGE).enableSearch(true).build();
+
+        param.prompt = message
+        val result = gen.call(param)
+        messageManager.add(result)
+
+        return result.output.choices[0].message.content
+    }
+}
 
 object AskAICommand : QQBotCommand {
     override val name = "ask-ai"
@@ -12,7 +36,9 @@ object AskAICommand : QQBotCommand {
     override val docs = "随机抓取幸运AI"
 
     override suspend fun handle(event: MessageCreateEvent) {
-        val response = QQBotApi.sendChannelMessage(event.channelId, SendChannelMessageRequest(content = "吃死你"))
+        val response = QQBotApi.sendChannelMessage(
+            event.channelId, SendChannelMessageRequest(content = AskAiUtils.getResponse("介绍一下你自己"))
+        )
         println(response.bodyAsText())
     }
 }
