0fa8ae1de14623e639e36a31b2b15c0aa3627096
hukening <kening.hu@outlook.com>
2024-03-03 10:52:34 +0800

    add initial prompt

--- src/main/kotlin/club/pisquad/uiharu/qqbot/command/AskAICommand.kt
+++ src/main/kotlin/club/pisquad/uiharu/qqbot/command/AskAICommand.kt
@@ -6,7 +6,9 @@ import club.pisquad.uiharu.qqbot.api.dto.SendChannelMessageRequest
 import club.pisquad.uiharu.qqbot.websocket.dto.MessageCreateEvent
 import com.alibaba.dashscope.aigc.generation.Generation
 import com.alibaba.dashscope.aigc.generation.models.QwenParam
+import com.alibaba.dashscope.common.Message
 import com.alibaba.dashscope.common.MessageManager
+import com.alibaba.dashscope.common.Role
 import com.alibaba.dashscope.utils.Constants
 import io.ktor.client.statement.*
 
@@ -16,6 +18,20 @@ internal object AskAiUtils {
 
     init {
         Constants.apiKey = AppConfiguration.getString("dashScopeApiKey")
+    }
+
+    init {
+        messageManager.add(
+            Message.builder().role(Role.SYSTEM.value).content(
+                """
+                    你叫作 Uiharu
+                    你是粉墨战队的 御用风纪委员
+                    粉墨战队是一个学生社团
+                    你负责协助粉墨战队的成员开展活动
+                """.trimIndent()
+            ).build()
+        )
+
     }
 
     fun getResponse(message: String): String {
