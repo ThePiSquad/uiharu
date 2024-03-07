a7fd9733b9ff593e3039fbc9017236b2c624c8b7
hukening <kening.hu@outlook.com>
2024-03-07 21:42:56 +0800

    fix: internal error :)

--- src/main/kotlin/club/pisquad/uiharu/qqbot/websocket/DispatchHandler.kt
+++ src/main/kotlin/club/pisquad/uiharu/qqbot/websocket/DispatchHandler.kt
@@ -7,21 +7,25 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.decodeFromJsonElement
 
-
 internal object DispatchHandler {
 
     private val eventConverter = Json { ignoreUnknownKeys = true }
     suspend fun handle(payload: PayloadBase<JsonElement>) {
         when (payload.t) {
-            EventType.MESSAGE_CREATE.value, EventType.MESSAGE_CREATE.value -> handleMessageCreate(
+            EventType.MESSAGE_CREATE.value, EventType.AT_MESSAGE_CREATE.value -> handleMessageCreate(
                 eventConverter.decodeFromJsonElement<MessageCreateEvent>(
                     payload.d!!
                 )
             )
+
+            else -> {
+                LOGGER.debug("Unhandled dispatch {}", payload)
+            }
         }
     }
 
     private suspend fun handleMessageCreate(event: MessageCreateEvent) {
+        LOGGER.debug("handling MessageCreateEvent")
         println(event)
     }
 
